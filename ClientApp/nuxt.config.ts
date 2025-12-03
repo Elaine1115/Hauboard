d67f9374158b326eae59bbd4d5bf17aa6837b5e2
Elaine1115 <e20051115@gmail.com>
2025-12-03 15:52:13 +0800

    change logo icon

--- ClientApp/nuxt.config.ts
+++ ClientApp/nuxt.config.ts
@@ -1,52 +1,46 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false, // 關閉 SSR，生成靜態網站
+  nitro: {
+    preset: 'static'
+  },
   future: {
     compatibilityVersion: 4
   },
-
   devtools: { enabled: true },
-
   app: {
     head: {
-      title: 'Glide.ai',
-      htmlAttrs: {
-        lang: 'en',
-      },
+      title: 'HAU BOARD INDUSTRIAL CO., LTD',
+      htmlAttrs: { lang: 'en' },
       meta: [
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
         { name: 'description', content: '' },
-        { name: 'format-detection', content: 'telephone=no' },
+        { name: 'format-detection', content: 'telephone=no' }
       ],
       link: [
-        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      ],
+        { rel: 'icon', type: 'image/webp', href: '/assets/images/logo/hauboard_icon.webp' },
+        { rel: 'apple-touch-icon', href: '/assets/images/logo/hauboard_icon.webp' }
+      ]
     }
   },
-
   css: ['~/assets/css/main.css'],
-
   modules: [
     '@nuxt/eslint',
     '@nuxtjs/tailwindcss',
     '@nuxt/fonts',
     '@nuxt/icon',
     '@pinia/nuxt',
-    '@nuxtjs/i18n',
+    '@nuxtjs/i18n'
   ],
-
   i18n: {
     locales: [
       { code: 'en', name: 'English', file: 'en.json' },
-      { code: 'zh', name: '中文', file: 'zh.json' },
+      { code: 'zh', name: '中文', file: 'zh.json' }
     ],
     defaultLocale: 'zh',
     strategy: 'no_prefix',
-    langDir: 'locales',
+    langDir: 'locales'
   },
-
-  compatibilityDate: '2025-01-06',
-
   routeRules: {
     '/': { redirect: '/home' }
   }
